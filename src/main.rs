ac1c658204fb8c8587910bf24508ce37faacb7ce
Steen Clarke <cmclarkey@gmail.com>
2016-09-30 16:05:20 -0700

    Sample each sequence and check if unique, if so log as 1, if not log as 0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
     let lines = "aggaggt\naggaggt\ngggacgt\ngggacgt\ngggactt".lines();
@@ -21,5 +22,22 @@ fn main() {
     rand::thread_rng().shuffle(&mut data_vector);
 
     println!("{:?}", data_vector);
+
+    let mut rarefaction = HashMap::new();
+    let mut log_hash = HashMap::new();
+
+    for x in 0..data_vector.len() {
+        if rarefaction.contains_key(data_vector[x]) {
+            *rarefaction.get_mut(data_vector[x]).unwrap() += 1;
+            log_hash.insert(x, 0);
+        } else {
+            rarefaction.insert(data_vector[x], 1);
+            log_hash.insert(x, 1);
+        }
+    }
+
+    for (sample, unique_status) in &log_hash {
+        println!("{}: {}", sample, unique_status);
+    }
 
 }
