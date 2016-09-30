44fc9158776524e03e1a3b43c30011da9fc0665a
Steen Clarke <cmclarkey@gmail.com>
2016-09-30 14:57:04 -0700

    Iterate over all sequences in array

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
     let data = ["aggaggt","aggaggt","gggacgt","gggacgt","gggactt"];
-    println!("The array has {} elements", data.len());
+    //println!("The array has {} elements", data.len());
+
+    //let mut times = data.len();
+    let mut x = 0;
+    let mut done = false;
+
+    while !done {
+        println!("The sequence is {}", data[x]);
+        x += 1;
+
+        if x == data.len() {
+            done = true;
+        }
+    }
 }
